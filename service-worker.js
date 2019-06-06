05b426f85d9e5c831ce543efcdd39f0b589f7ce5
xzava <micheal.hall@gmail.com>
2019-06-06 21:46:57 +1200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,3 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
@@ -100,27 +83,31 @@ self.addEventListener('activate', (evt) => {
   self.clients.claim();
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
+self.addEventListener('fetch', (evt) => {
+	if (evt.request.url.includes('/forecast/')) {
+	  console.log('[Service Worker] Fetch (data)', evt.request.url);
+	  evt.respondWith(
+	      caches.open(DATA_CACHE_NAME).then((cache) => {
+	        return fetch(evt.request)
+	            .then((response) => {
+	              // If the response was good, clone it and store it in the cache.
+	              if (response.status === 200) {
+	                cache.put(evt.request.url, response.clone());
+	              }
+	              return response;
+	            }).catch((err) => {
+	              // Network request failed, try to get it from the cache.
+	              return cache.match(evt.request);
+	            });
+	      }));
+	  return;
+	}
+	evt.respondWith(
+	    caches.open(CACHE_NAME).then((cache) => {
+	      return cache.match(evt.request)
+	          .then((response) => {
+	            return response || fetch(evt.request);
+	          });
+	    })
+	);
 });
