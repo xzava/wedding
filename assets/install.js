19c4d2996ca4c5269e39b0004d762498cf8ec719
xzava <micheal.hall@gmail.com>
2019-06-06 23:49:10 +1200

    Update install.js

--- assets/install.js
+++ assets/install.js
@@ -22,7 +22,7 @@ const installButton = document.getElementById('butInstall');
 installButton.addEventListener('click', installPWA);
 
 // CODELAB: Add event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+// window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
 
 /**
@@ -33,8 +33,8 @@ window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
  */
 function saveBeforeInstallPromptEvent(evt) {
   // CODELAB: Add code to save event & show the install button.
-  deferredInstallPrompt = evt;
-  installButton.removeAttribute('hidden');
+//   deferredInstallPrompt = evt;
+//   installButton.removeAttribute('hidden');
 
 }
 
@@ -46,24 +46,24 @@ function saveBeforeInstallPromptEvent(evt) {
  */
 function installPWA(evt) {
   // CODELAB: Add code show install prompt & hide the install button.
-  deferredInstallPrompt.prompt();
+//   deferredInstallPrompt.prompt();
   // CODELAB: Log user response to prompt.
-  deferredInstallPrompt.userChoice
-      .then((choice) => {
-        if (choice.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt', choice);
-        } else {
-          console.log('User dismissed the A2HS prompt', choice);
-        }
-        deferredInstallPrompt = null;
-      });
+//   deferredInstallPrompt.userChoice
+//       .then((choice) => {
+//         if (choice.outcome === 'accepted') {
+//           console.log('User accepted the A2HS prompt', choice);
+//         } else {
+//           console.log('User dismissed the A2HS prompt', choice);
+//         }
+//         deferredInstallPrompt = null;
+//       });
   // Hide the install button, it can't be called twice.
-  evt.srcElement.setAttribute('hidden', true);
+//   evt.srcElement.setAttribute('hidden', true);
 
 }
 
 // CODELAB: Add event listener for appinstalled event
-window.addEventListener('appinstalled', logAppInstalled);
+// window.addEventListener('appinstalled', logAppInstalled);
 /**
  * Event handler for appinstalled event.
  *   Log the installation to analytics or save the event somehow.
@@ -72,6 +72,6 @@ window.addEventListener('appinstalled', logAppInstalled);
  */
 function logAppInstalled(evt) {
   // CODELAB: Add code to log the event
-  console.log('Weather App was installed.', evt);
+//   console.log('Weather App was installed.', evt);
 
 }
